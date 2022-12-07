17e197478a3d14f4da5e0d16201cfbc100f0379b
nampham <nampham@gmail.com>
2022-12-07 13:38:39 +0700

    f3

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.get("/", (req,res)=> {
+    res.send('Nam Pham');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at : http://localhost: ${PORT}`);
+})
